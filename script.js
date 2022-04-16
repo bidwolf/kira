3245f99b106d0ae4bdb2f960949e7435638933ba
Henrique de Paula Rodrigues <tec.henriquedepaula@gmail.com>
2022-04-16 20:09:49 -0300

    Adicionando barra de navegação

---
+++ script.js
@@ -0,0 +1,14 @@
+let toggleButton = document.querySelector('[class="navbar-toggler collapsed"]');
+let toggleElements = document.getElementById(toggleButton.getAttribute('aria-controls'));
+toggleButton.addEventListener("click", function(e) {
+    if (this.getAttribute('aria-expanded') === 'false') {
+        toggleElements.classList.add("show");
+        this.setAttribute('aria-expanded', 'true');
+        this.classList.remove("collapsed");
+    } else {
+        toggleElements.classList.remove("show");
+        this.setAttribute('aria-expanded', 'false');
+        this.classList.add("collapsed");
+    }
+
+});
